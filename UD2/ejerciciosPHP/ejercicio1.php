e30b99e62de76fe6d74299edeec07a04e25eac26
pgtrilla <gracia.pablo.smr@gmail.com>
2021-09-23 17:56:55 +0100

    Ejercicios UD2 em proceso

---
+++ UD2/ejerciciosPHP/ejercicio1.php
@@ -0,0 +1,11 @@
+<?php
+
+$x = 144;
+$y = 999;
+
+echo 'Variable x = ' . $x . '<br>';
+echo 'Variable y = ' . $y. '<br>';
+echo 'Suma ' . $x + $y. '<br>';
+echo 'Resta ' . $x - $y. '<br>';
+echo 'División ' . $x / $y. '<br>';
+echo 'Multiplicación ' . $x * $y. '<br>';
---
+++ UD2/ejerciciosPHP/ejercicio2.php
@@ -0,0 +1,7 @@
+<?php
+
+$nombre = 'Pablo';
+$direccion = 'Carlos Oriz Garcia';
+$mail = 'gracia.pablo.smr@gmail.com';
+
+echo $nombre .', Calle: ' . $direccion . ', Mail: ' . $mail  ;
