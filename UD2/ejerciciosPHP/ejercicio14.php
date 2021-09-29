7d0f858d53674d5f7ef426d97d6ecc7167a12fdb
pgtrilla <gracia.pablo.smr@gmail.com>
2021-09-29 15:35:47 +0100

    branch

---
+++ UD2/ejerciciosPHP/ejercicio14.php
@@ -0,0 +1,21 @@
+<?php
+
+$str = 'catac';
+
+function is_palindromo( $str ){
+    
+    $bPalindromo = false;
+
+    for( $i = 0; $i < count($str); $i++ ){
+        
+    }
+    
+    return $bPalindromo;
+}
+
+
+
+// echo o_segundo_grado( $a, $b, $c ); 
+print_r(o_segundo_grado( $a, $b, $c )); 
+
+ ?>
